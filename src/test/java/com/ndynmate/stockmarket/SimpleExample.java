34774106beaa8b7f51ce05dc3cc1767cd862c809
czujko <55107723+czujko@users.noreply.github.com>
2022-02-10 22:30:32 +0100

    Test *

--- src/test/java/com/ndynmate/stockmarket/SimpleExample.java
+++ src/test/java/com/ndynmate/stockmarket/SimpleExample.java
@@ -2,7 +2,11 @@ package com.ndynmate.stockmarket;
 
 import com.ndynmate.stockmarket.constant.Currency;
 import com.ndynmate.stockmarket.impl.CoinGeckoApiClientImpl;
+import com.ndynmate.stockmarket.modelo.CoinFullData;
+import com.ndynmate.stockmarket.modelo.CoinMarkets;
+import com.ndynmate.stockmarket.modelo.CoinTickerById;
 
+import java.util.List;
 import java.util.Map;
 
 public class SimpleExample {
@@ -10,20 +14,23 @@ public class SimpleExample {
         CoinGeckoApiClient client = (CoinGeckoApiClient) new CoinGeckoApiClientImpl();
 
         Map<String, Map<String, Double>> bitcoin = client.getPrice("bitcoin", Currency.USD,false,true,true,true);
-        Map<String, Map<String, Double>> cardano = client.getPrice("cardano",Currency.EUR);
-        Map<String, Map<String, Double>> ethereum = client.getPrice("ethereum",Currency.EUR);
+        /*Map<String, Map<String, Double>> cardano = client.getPrice("cardano",Currency.EUR);
+        Map<String, Map<String, Double>> ethereum = client.getPrice("ethereum",Currency.EUR);*/
+        CoinTickerById bitcoin2 = client.getCoinTickerById("bitcoin");
+
 
         System.out.println(bitcoin);
-        System.out.println(cardano);
-        System.out.println(ethereum);
+        System.out.println(bitcoin2);
+        /*System.out.println(cardano);
+        System.out.println(ethereum);*/
 
         double bitcoinPrice = bitcoin.get("bitcoin").get(Currency.USD);
-        double cardanoPrice = cardano.get("cardano").get(Currency.EUR);
-        double ethereumPrice = ethereum.get("ethereum").get(Currency.EUR);
+        /*double cardanoPrice = cardano.get("cardano").get(Currency.EUR);
+        double ethereumPrice = ethereum.get("ethereum").get(Currency.EUR);*/
 
         System.out.println(bitcoinPrice);
-        System.out.println(cardanoPrice);
-        System.out.println(ethereumPrice);
+        /*System.out.println(cardanoPrice);
+        System.out.println(ethereumPrice);*/
 
         client.shutdown();
     }
